5ea2c3e84f8d642b3723f120a8186ed5def0b755
cris-gar <cristian.garrido@ceinf.cl>
2015-02-24 18:45:05 -0300

    se muestra el correo en el formulario de registro si es que sale algun error, esto es para ayudar a los usuarios no tener que reescribir

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -2,6 +2,16 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def facebook
     @usuario = Usuario.from_omniauth(request.env["omniauth.auth"])
-    sign_in_and_redirect @usuario
+
+    if @usuario.persisted? #Validacion de creacion de Usuario
+      sign_in_and_redirect @usuario
+    else
+      #session[:omniauth_errors] = @usuario.errors.full_messages.to_sentence unless @usuario.save
+
+      #session[:omniauth_data] = data
+
+      redirect_to new_usuario_registration_url # redireccion para la vista de registro para corregir errores
+    end
+  #  sign_in_and_redirect @usuario
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
 
   def get_email_oauth #helper para autocompletar los datos por registro en omniauth
-    if session[:omniauth_data]
-      session[:omniauth_data][:email]
+    if devise_error_messages!
+      @usuario.email
     else
-      ""
+
     end
   end
 end
